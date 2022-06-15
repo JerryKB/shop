8316b9383bf81a0306aca92f2e2d5bfa0167dff6
JerryKB <2585388719@qq.com>
2022-06-16 00:06:10 +0800

    菜单

---
+++ shop-start/src/main/java/com/shop/pojo/UserBean.java
@@ -0,0 +1,16 @@
+package com.shop.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserBean {
+    private String username;
+    private String password;
+    private String email;
+    private String real_name;
+
+}
